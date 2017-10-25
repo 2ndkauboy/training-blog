1861f11a69a8ae2883cb523b7c7596777bc786f5
Bernhard Kau <bernhard.kau@vcat.de>
2017-10-25 12:34:38 +0200

    Add BlogController with indexAction

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use \Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends Controller
+{
+    /**
+     * @Route("/", name="blog_index")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function indexAction(Request $request): Response
+    {
+        return $this->render('index.html.twig');
+    }
+}
